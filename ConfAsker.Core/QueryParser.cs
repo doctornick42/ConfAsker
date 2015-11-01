8c95349186adaf6f0c96af796b7c86160bf882e9
doctornick <dmitry.bogomolov@iage.net>
2015-11-02 01:21:09 +0300

    default namespaces were changed

---
+++ ConfAsker.Core/QueryParser.cs
@@ -0,0 +1,18 @@
+﻿using ConfAsker.Core;
+using ConfAsker.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfAsker.Core
+{
+    public class QueryParser : IQueryParser
+    {
+        public Query ParseQuery(string queryString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
